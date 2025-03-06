8972fafeec3e8814729ec7490e547bd74365a614
smoaking90 <cwsmoak90@gmail.com>
2025-03-06 10:28:24 -0500

    adding helper functions

--- helpers.php
+++ helpers.php
@@ -42,6 +42,24 @@ function loadPartial($name){
     }
 }
 
-function foo(){
-    echo 'bar';
+/**
+ * inspect a value(s)
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value){
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+}
+
+/**
+ * inspect a value(s) and die
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value){
+    echo "<pre>";
+    die(var_dump($value));
+    echo "</pre>";
 }
