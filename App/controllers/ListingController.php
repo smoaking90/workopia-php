44de25288f70b08ea4bafe55903e3da97872c8f0
smoaking90 <cwsmoak90@gmail.com>
2025-03-12 17:48:58 +0100

    validating form data

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -87,7 +87,26 @@ class ListingController{
         $newListingData['user_id'] = 1;
 
         $newListingData = array_map('sanitize', $newListingData);
+
+        $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach($requiredFields as $field){
+            if(empty($newListingData[$field]) || !Validation::string($newListingData[$field])){
+                $errors[$field] = ucfirst($field) . ' is required.';
+            }
+        }
         
-        inspectAndDie($newListingData);
+        if(!empty($errors)){
+            // Reload view with errors
+            loadView('listings/create', [
+                'errors' => $errors,
+                'listing' => $newListingData
+            ]);
+        }else{
+            // Submit data
+            echo 'Success';
+        }
     }
 }
--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -16,12 +16,18 @@ loadPartial('top-banner');
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
             Job Info
           </h2>
+          <?php if(isset($errors)) : ?>
+              <?php foreach($errors as $error) : ?>
+                  <div class="message bg-red-100 my-3"><?= $error ?></div>
+                <?php endforeach ; ?>
+            <?php endif ; ?>
           <div class="mb-4">
             <input
               type="text"
               name="title"
               placeholder="Job Title"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?= $listing['title'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -29,7 +35,7 @@ loadPartial('top-banner');
               name="description"
               placeholder="Job Description"
               class="w-full px-4 py-2 border rounded focus:outline-none"
-            ></textarea>
+            ><?= $listing['description'] ?? ''?></textarea>
           </div>
           <div class="mb-4">
             <input
@@ -37,6 +43,7 @@ loadPartial('top-banner');
               name="salary"
               placeholder="Annual Salary"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['salary'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -45,6 +52,7 @@ loadPartial('top-banner');
               name="requirements"
               placeholder="Requirements"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['requirements'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -53,6 +61,7 @@ loadPartial('top-banner');
               name="benefits"
               placeholder="Benefits"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['benefits'] ?? ''?>"
             />
           </div>
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
@@ -64,6 +73,7 @@ loadPartial('top-banner');
               name="company"
               placeholder="Company Name"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['company'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -72,6 +82,7 @@ loadPartial('top-banner');
               name="address"
               placeholder="Address"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['address'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -80,6 +91,7 @@ loadPartial('top-banner');
               name="city"
               placeholder="City"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['city'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -88,6 +100,7 @@ loadPartial('top-banner');
               name="state"
               placeholder="State"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['state'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -96,6 +109,7 @@ loadPartial('top-banner');
               name="phone"
               placeholder="Phone"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['phone'] ?? ''?>"
             />
           </div>
           <div class="mb-4">
@@ -104,6 +118,7 @@ loadPartial('top-banner');
               name="email"
               placeholder="Email Address For Applications"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?=$listing['email'] ?? ''?>"
             />
           </div>
           <button
