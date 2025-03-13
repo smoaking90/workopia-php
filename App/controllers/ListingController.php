5629a7fd07b88ed783dd7ff34d99e257b0a76344
smoaking90 <cwsmoak90@gmail.com>
2025-03-13 18:28:38 +0100

    flash messaging

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -160,6 +160,9 @@ class ListingController{
         
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
+        // Set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
+
         redirect('/listings');
     }
 }
