17ef1ae2ef95088fde5342ae4cd5aa87efbf86ca
smoaking90 <cwsmoak90@gmail.com>
2025-03-14 15:40:56 +0100

    adding user controller and views

--- routes.php
+++ routes.php
@@ -11,6 +11,9 @@ $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
 
+$router->get('auth/register', 'UserController@create');
+$router->get('auth/login', 'UserController@login');
+
 
 
 
