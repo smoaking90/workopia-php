0f9c9415b1d5c3b251aecf0b87e624b9efde90c9
smoaking90 <cwsmoak90@gmail.com>
2025-03-14 16:31:39 +0100

    validating registration and error partial

--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -6,7 +6,10 @@
 <div class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
     <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
-    <form>
+    <?= loadPartial('errors', [
+            'errors' => $errors ?? []
+          ]) ?>
+    <form method="POST" action="/auth/register">
         <div class="mb-4">
         <input
             type="text"
--- helpers.php
+++ helpers.php
@@ -33,10 +33,11 @@ function loadView($name, $data = []){
  * @param string $name
  * @return void
  */
-function loadPartial($name){
+function loadPartial($name, $data = []){
     $partialPath = basePath("App/views/partials/{$name}.php");
 
     if(file_exists($partialPath)){
+        extract($data);
         require $partialPath;
     }else{
         echo "Partial {$name} not found.";
