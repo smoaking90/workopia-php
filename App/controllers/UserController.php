f64622487eb568d31d65e1c1a7dee4653a63ee18
smoaking90 <cwsmoak90@gmail.com>
2025-03-16 18:57:15 +0100

    logging out killings sessions

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -117,4 +117,19 @@ class UserController{
 
         redirect('/');
     }
+
+    /**
+     * Logout user and kill session
+     * 
+     * @return void
+     */
+    public function logout(){
+        Session::clearAll();
+
+        $params = session_get_cookie_params();
+
+        setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
+
+        redirect('/');
+    }
 }
--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -10,10 +10,10 @@ use Framework\Session;
         <nav class="space-x-4">
           <?php if(Session::has('user')) : ?>
             <div class="flex justify-between items-center gap-4">
-              <div>
+              <div class="text-blue-500">
                 Welcome <?= Session::get('user')['name'] ?>
               </div>
-            <form>
+            <form method="POST" action="/auth/logout">
               <button type="submit" class="text-white inline hover:underline">Logout</button>
             </form>
             <a href="/listings/create"
